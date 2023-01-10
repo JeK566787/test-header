8decaa070553dcdd1e5c60130e28ec17120d3d35
Evgeniy Pavlichenko <yevheniipavl@gmail.com>
2023-01-10 23:43:37 +0200

    up

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const dropdownRef = document.querySelector('.dropdown');
 const dotsRef = document.querySelector('.dots');
 const basketContainerRef = document.querySelector('.basket-container');
-const fullBasketIconRef = document.querySelector('.full-basket-icon');
+// const fullBasketIconRef = document.querySelector('.full-basket-icon');
 
 dotsRef.addEventListener('click', onDotsClick);
 basketContainerRef.addEventListener('click', onBasketClick);
 
 function onBasketClick() {
   basketContainerRef.classList.toggle('full-basket');
-  fullBasketIconRef.classList.toggle('full-basket-icon');
+  // fullBasketIconRef.classList.toggle('full-basket-icon');
 }
 
 function onDotsClick(e) {
